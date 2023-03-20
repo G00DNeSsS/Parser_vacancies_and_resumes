cf8c5bb90caa81e0b7749edf7493f87e381cce8e
G00DNeSsS <vasilev21042001@gmail.com>
2023-03-20 22:40:50 +0300

    last version

--- gui.py
+++ gui.py
@@ -8,10 +8,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from seleniumwire import webdriver
 import time
 import json
 import requests
-import pprint
+
 
 
 def requests_token(token):
@@ -24,25 +25,12 @@ def requests_token(token):
 	with open('data.json', 'w', encoding='utf-8') as f:
 		json.dump(data, f, ensure_ascii=False, indent=4)
 
-def process_browser_logs_for_network_events(logs):
-    """
-    Return only logs which have a method that start with "Network.response", "Network.request", or "Network.webSocket"
-    since we're interested in the network events specifically.
-    """
-    for entry in logs:
-        log = json.loads(entry["message"])["message"]
-        if (
-               
-                "Network.request" in log["method"]
-               
-        ):
-            yield log
-
-def selenium():
+def selenium(window):
 	capabilities = DesiredCapabilities.CHROME
 	capabilities["goog:loggingPrefs"] = {"performance": "ALL"}  # chromedriver 75+
 	chrome_options = Options()
 	chrome_options = webdriver.ChromeOptions()
+	chrome_options.add_argument('ignore-certificate-errors')
 	chrome_options.add_experimental_option("detach", True)
 	browser = webdriver.Chrome('chromedriver.exe', chrome_options=chrome_options, desired_capabilities=capabilities)
 	browser.get("https://hh.ru/login")
@@ -51,31 +39,41 @@ def selenium():
 	if link == 'https://spb.hh.ru/?hhtmFrom=account_login':
 		print(link)
 		browser.get("https://hh.ru/oauth/authorize?response_type=code&client_id=M3RJAUA7MKSI9OFB32695SIBMR92DQMAQ54S9N97NPFC84F7QTE4G4V5PVKNG0BR")
-		time.sleep(20)
-		logs = browser.get_log("performance")
-		events = process_browser_logs_for_network_events(logs)
-		with open("log_entries.txt", "wt") as out:
-			for event in events:
-				pprint.pprint(event, stream=out)
+		time.sleep(10)
+		for request in browser.requests:
+			if request.response:
+				if request.url == 'https://hh.ru/oauth/authorize?response_type=code&client_id=M3RJAUA7MKSI9OFB32695SIBMR92DQMAQ54S9N97NPFC84F7QTE4G4V5PVKNG0BR': 
+					if request.response.headers['location'] is not None:
+						head, sep, tail = request.response.headers['location'].partition('myapp://auth?code=')
+						requests_token(tail)	
+					else:
+						continue
+		browser.close()
+		window['Vac'].update(visible=True)
+		window['Resume'].update(visible=True)	
 	if link == 'https://spb.hh.ru/':
-		print(link)
 		browser.get("https://hh.ru/oauth/authorize?response_type=code&client_id=M3RJAUA7MKSI9OFB32695SIBMR92DQMAQ54S9N97NPFC84F7QTE4G4V5PVKNG0BR")
-		time.sleep(20)
-		logs = browser.get_log("performance")
-		with open("log_entries.txt", "wt") as out:
-			for event in events:
-				pprint.pprint(event, stream=out)
+		time.sleep(10)
+		for request in browser.requests:
+			if request.response:
+				if request.url == 'https://hh.ru/oauth/authorize?response_type=code&client_id=M3RJAUA7MKSI9OFB32695SIBMR92DQMAQ54S9N97NPFC84F7QTE4G4V5PVKNG0BR': 
+					if request.response.headers['location'] is not None:
+						head, sep, tail = request.response.headers['location'].partition('myapp://auth?code=')
+						requests_token(tail)	
+					else:
+						continue
+		browser.close()				
+		window['Vac'].update(visible=True)
+		window['Resume'].update(visible=True)		
 
 def init():
 	sg.theme('Dark Amber') 
 	layout = [[sg.Text('Parser HH.RU',font='Helvetica 26 bold')],
-	[sg.Button('Войти в личный кабинет', button_color=('black','white'), size=(43,2),border_width=4,key='LK', font='Helvetica 10 bold')],
-	[sg.InputText(key='token',size=(50, 2))],
-	[sg.Button('Подтвердить', button_color=('black','white'), size=(43,2),border_width=4,key='Accpets', font='Helvetica 10 bold',visible = True)],
+	[sg.Button('Войти в личный кабинет Head Hunter', button_color=('black','white'), size=(43,2),border_width=4,key='LK', font='Helvetica 10 bold')],
 	[sg.Button('Вакансии', button_color=('black','white'), size=(30,2),border_width=4,key='Vac', font='Helvetica 10 bold',visible = False),sg.Button('Резюме', button_color=('black','white'),border_width=4, size=(30,2), key='Resume', font='Helvetica 10 bold',visible = False)]
 	]
 	
-	window = sg.Window('Parser HH.RU', layout,size=(1000, 600),element_justification='c',margins=(0,150))
+	window = sg.Window('Parser HH.RU', layout,size=(1000, 600),element_justification='c',margins=(0,180))
 	while True:
 		event, values = window.read()
 		if event == sg.WIN_CLOSED or event == '-Stop-': # if user closes window or clicks cancel
@@ -87,11 +85,7 @@ def init():
 			window.close()
 			import parse_resume
 		elif event == "LK":
-			th = Thread(target=selenium)
-			th.start()
-		elif event == "Accpets":
-			requests_token(values['token'])	
-			window['Vac'].update(visible=True)
-			window['Resume'].update(visible=True)			
+			th = Thread(target=selenium, args=(window,))
+			th.start()					
 	window.close()
 init()
